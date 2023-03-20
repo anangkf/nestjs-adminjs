44e1090c8b40dfd8b82e4e750523a9366596f125
anangkf_ <gonanggoneng@gmail.com>
2023-03-20 22:40:53 +0700

    setup adminjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,57 @@
+import { AdminModule } from '@adminjs/nestjs';
 import { Module } from '@nestjs/common';
+import * as AdminJSPrisma from '@adminjs/prisma';
+import AdminJS from 'adminjs';
 import { ProductModule } from './product/product.module';
+import { PrismaService } from './prisma.service';
+import { DMMFClass } from '@prisma/client/runtime';
+
+// register prisma adapter
+AdminJS.registerAdapter(AdminJSPrisma);
+
+const DEFAULT_ADMIN = {
+  email: 'admin@borneo.biz.id',
+  password: 'admin123',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    AdminModule.createAdminAsync({
+      useFactory: () => {
+        const prisma = new PrismaService();
+        const dmmf = (prisma as any)._baseDmmf as DMMFClass;
+        return {
+          adminJsOptions: {
+            rootPath: '/admin',
+            resources: [
+              {
+                resource: { model: dmmf.modelMap.Product, client: prisma },
+                options: {},
+              },
+            ],
+          },
+          auth: {
+            authenticate,
+            cookieName: 'adminjs',
+            cookiePassword: 'secret',
+          },
+          sessionOptions: {
+            resave: true,
+            saveUninitialized: true,
+            secret: 'secret',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
