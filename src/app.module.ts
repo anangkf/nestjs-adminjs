1daadf01d6519052312b1b40801ecae6a6604e72
anangkf_ <gonanggoneng@gmail.com>
2023-04-03 14:45:14 +0700

    remove adminjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,65 +1,10 @@
-import { AdminModule } from '@adminjs/nestjs';
 import { Module } from '@nestjs/common';
-import * as AdminJSPrisma from '@adminjs/prisma';
-import AdminJS from 'adminjs';
 import { ProductModule } from './product/product.module';
-import { PrismaService } from './prisma.service';
-import { DMMFClass } from '@prisma/client/runtime';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
-// register prisma adapter
-AdminJS.registerAdapter(AdminJSPrisma);
-
-const DEFAULT_ADMIN = {
-  email: 'admin@borneo.biz.id',
-  password: 'admin123',
-};
-
-const authenticate = async (email: string, password: string) => {
-  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
-    return Promise.resolve(DEFAULT_ADMIN);
-  }
-  return null;
-};
-
 @Module({
-  imports: [
-    ProductModule,
-    AdminModule.createAdminAsync({
-      useFactory: () => {
-        const prisma = new PrismaService();
-        const dmmf = (prisma as any)._baseDmmf as DMMFClass;
-        return {
-          adminJsOptions: {
-            rootPath: '/admin',
-            resources: [
-              {
-                resource: { model: dmmf.modelMap.Product, client: prisma },
-                options: {},
-              },
-              {
-                resource: { model: dmmf.modelMap.User, client: prisma },
-                options: {},
-              },
-            ],
-          },
-          auth: {
-            authenticate,
-            cookieName: 'adminjs',
-            cookiePassword: 'secret',
-          },
-          sessionOptions: {
-            resave: true,
-            saveUninitialized: true,
-            secret: 'secret',
-          },
-        };
-      },
-    }),
-    UserModule,
-    AuthModule,
-  ],
+  imports: [ProductModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
